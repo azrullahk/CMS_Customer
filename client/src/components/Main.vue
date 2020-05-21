e3a0cb9d990c5952fbf3e568c0082aa20743e84b
azrullahk <azrullahkainage@gmail.com>
2020-05-22 06:35:33 +0700

    create landing page

---
+++ client/src/components/Main.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <!-- over the fold -->
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-dark" id="mainBanner">
+      <div class="col-md-6 p-lg-6 mx-auto my-6">
+        <h1 id="textMain">Goes beyond the Pro's</h1>
+        <p class=""> <button class="btn btn-link"><h4>Lebih Lanjut ></h4></button> <button class="btn btn-link"><h4>Beli ></h4></button> </p>
+      </div>
+    </div>
+    <!-- banner 2 -->
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-dark" id="banner2">
+      <div class="col-md-6 p-lg-6 mx-auto my-6">
+        <div id="textBanner2"> </div>
+        <p class=""> <button class="btn btn-link"><h4>Lebih Lanjut ></h4> </button> <button class="btn btn-link"><h4>Beli ></h4></button> </p>
+      </div>
+    </div>
+
+    <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+      <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+        <div class="my-3 py-3">
+          <h2 class="display-5">Another headline</h2>
+          <p class="lead">And an even wittier subheading.</p>
+        </div>
+        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+    </div>
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 p-3">
+      <h2 class="display-5">Another headline</h2>
+      <p class="lead">And an even wittier subheading.</p>
+    </div>
+     <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+    </div>
+  </div>
+
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 p-3">
+      <h2 class="display-5">Another headline</h2>
+      <p class="lead">And an even wittier subheading.</p>
+    </div>
+    <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+  </div>
+  <div class="bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+    <div class="my-3 py-3">
+      <h2 class="display-5">Another headline</h2>
+      <p class="lead">And an even wittier subheading.</p>
+    </div>
+    <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+  </div>
+</div>
+
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 p-3">
+      <h2 class="display-5">Another headline</h2>
+      <p class="lead">And an even wittier subheading.</p>
+    </div>
+    <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+  </div>
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 py-3">
+      <h2 class="display-5">Another headline</h2>
+      <p class="lead">And an even wittier subheading.</p>
+    </div>
+    <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+  </div>
+</div>
+
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 p-3">
+      <h2 class="display-5">Another headline</h2>
+      <p class="lead">And an even wittier subheading.</p>
+    </div>
+    <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+  </div>
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 py-3">
+      <h2 class="display-5">Another headline</h2>
+      <p class="lead">And an even wittier subheading.</p>
+    </div>
+    <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style scoped>
+  #mainBanner {
+    background-image: url('https://i.ytimg.com/vi/5NoZ39sforc/maxresdefault.jpg');
+    background-size:auto;
+    color: whitesmoke;
+    height: 90vh;
+    margin: 0;
+  }
+  #textMain {
+    margin-top: 270px;
+  }
+  #banner2 {
+    background-image: url('https://media.idownloadblog.com/wp-content/uploads/2020/04/iPad-Pro-float-ad-banner.jpg');
+    background-size: cover;
+    height: 100vh;
+  }
+  #textBanner2 {
+    margin-top: 420px;
+  }
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+<nav class="site-header sticky-top py-1">
+  <div class="container d-flex flex-column flex-md-row justify-content-between">
+    <a class="py-2" href="#" aria-label="Product">
+      <span>Apple.ID</span>
+    </a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Iphone</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Ipad</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">MacBook</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Mac Pro</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Airpods</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Accessories</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
